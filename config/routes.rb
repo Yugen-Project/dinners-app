7ad740e56579101bd264a9c13d1a20a343b2645a
Muna-Lombe <3223352545@qq.com>
2021-11-17 14:36:45 +0800

    branch name incorrect, work-done: 1. configured routes, 2. added dynamic click to restaurant#index page, 3. configured route from restaurant#index page to restaurant#show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # root 'restaurant/index'
-  get 'restaurant/index'
-  get 'restaurant/show'
-  get 'restaurant/create'
-  get 'restaurant/update'
-  get 'restaurant/destroy'
+  root to: "restaurant#index"
+  # get 'restaurants', to: "restaurant#index", as: "root"
+  get 'restaurant/:id', to: "restaurant#show", as: "show"
+  post "restaurant/:id", to: "restaurant#create", as: "new" 
+  post "restaurant/:id", to: "restaurant#update", as: "edit" 
+  post 'restaurant/:id', to: "restaurant#destroy", as: "delete"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
